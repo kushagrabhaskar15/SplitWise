4faba01a880b02bf6a5f33ee07b1cf44138f2109
kushagrabhaskar15 <kushagrabhaskar05@gmail.com>
2026-09-19 22:01:21 +0530

    Initial Commit

---
+++ SplitWise-backend/src/test/java/com/kushagrabhaskar/SplitWise/SplitWiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kushagrabhaskar.SplitWise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplitWiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
